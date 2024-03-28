04c62507839f0690e43302dc23da7e576fa50bdf
kacper <k4cper.petelicki@gmail.com>
2024-03-28 16:03:25 +0100

    add

---
+++ validate/src/App.js
@@ -0,0 +1,77 @@
+import logo from './logo.svg';
+import './App.css';
+import FormInput from './components/FormInput';
+import { useState } from 'react';
+
+function App() {
+  const [values,setValues]= useState({
+    username:"",
+    email:"",
+    birthday:"",
+    password:"",
+    confirmPassword:"",
+  })
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      label: "Username",
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      label: "Email",
+
+    },
+    {
+      id: 3,
+      name: "birthday",
+      type: "date",
+      placeholder: "Birthday",
+      label: "Birthday",
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      label: "Password"
+    },
+    {
+      id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Confirm Password",
+      label: "Confirm Password"
+     
+    },
+  ];
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const onChange=(e)=>{
+    setValues({ ...values, [e.target.name]: e.target.value });
+  }
+  console.log(values)
+  return (
+    
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        {inputs.map((input)=>(
+           <FormInput key={input.id} {...input} value={values[input.name]}
+           onChange={onChange}/>
+        ))}
+        
+     
+        <button>Submit</button>
+      </form>
+    
+    </div>
+  );
+}
+
+export default App;
